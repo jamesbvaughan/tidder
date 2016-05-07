af8b3b5cbd902497a60483c8e329be13c3aec6c4
James Vaughan <james@jamesbvaughan.com>
2016-05-06 18:10:35 -0700

    added sort, made code better

--- js/script.js
+++ js/script.js
@@ -1,107 +1,114 @@
+// Tidder ====================================================================
+//
+// by James Vaughan
 "use strict";
 
-const handleYoutubeURL = function (url) {
-	let video_id = '';
-	let urls = [url];
-	let prefixes = [
-		'http://www.youtube.com/watch?v=',
-		'https://www.youtube.com/watch?v=',
-		'http://www.youtu.be/',
-		'https://www.youtu.be/'
-	];
-	if (url.includes('v=')) {
-		video_id = url.split('v=')[1];
-		let ampersandPosition = video_id.indexOf('&');
-		if (ampersandPosition != -1) {
-			video_id = video_id.substring(0, ampersandPosition);
-		}
-		if (video_id !== "") {
-			for (let prefix of prefixes)
-				if (prefix + video_id != url)
-					urls.push(prefix + video_id);
-		}
-	}
-	return urls;
-};
-
-const handleYoutubeShortURL = function (url) {
-	let video_id = '';
-	let urls = [url];
-	let prefixes = [
-		'http://www.youtube.com/watch?v=',
-		'https://www.youtube.com/watch?v=',
-		'http://www.youtu.be/',
-		'https://www.youtu.be/'
-	];
-	if (url.length > 24) {
-		video_id = url.split('be/')[1];
-		let ampersandPosition = video_id.indexOf('&');
-		if (ampersandPosition != -1) {
-			video_id = video_id.substring(0, ampersandPosition);
-		}
-		if (video_id !== "") {
-			for (let prefix of prefixes)
-				if (prefix + video_id != url)
-					urls.push(prefix + video_id);
-		}
-	}
-	return urls;
-};
-
-const getURLs = function (url) {
-	if (!url.includes('http')) {
-		console.log("Hey, that's not a URL.");
-		return [];
-	}
-	let urls = [url];
-	if (url.includes('youtube.com')) {
-		urls = handleYoutubeURL(url);
-	} else if (url.includes('youtu.be')) {
-		urls = handleYoutubeShortURL(url);
-	}
-	return urls;
-};
+// Templates =================================================================
 
 const linkTemplate = function (postData) {
 	return `
-	<a href="http://reddit.com${postData.permalink}" class="list-group-item" target="_blank">
-		<span class="badge">${postData.score}</span>
-		<h4 class="list-group-item-heading">${postData.title}</h4>
-		<p class="list-group-item-text">submitted to <b>/r/${postData.subreddit}</b> by <i>${postData.author}</i> ${moment.unix(postData.created_utc).fromNow()} [${postData.num_comments} comments]</p>
-	</a>`;
+		<a href="http://reddit.com${postData.permalink}"
+				class="list-group-item" target="_blank">
+			<span class="badge">${postData.score}</span>
+			<h4 class="list-group-item-heading">${postData.title}</h4>
+			<p class="list-group-item-text">submitted to <b>/r/${postData.subreddit}</b> by <i>${postData.author}</i> ${moment.unix(postData.created_utc).fromNow()} [${postData.num_comments} comments]</p>
+		</a>`;
 };
 
-const spinDiv = document.getElementById("spinner");
-const spinner = new Spinner().spin(spinDiv);
+
+// Functions =================================================================
+
+const getURLs = function (url) {
+	if (url.includes('youtube.com') || url.includes('youtu.be')) {
+		let video_id = url.split('v=')[1].split('&')[0];
+		let urls = [];
+		let prefixes = [
+			'http://www.youtube.com/watch?v=',
+			'https://www.youtube.com/watch?v=',
+			'http://www.youtu.be/',
+			'https://www.youtu.be/'
+		];
+		if (url.includes('youtu.be'))
+			video_id = url.split('be/')[1].split('&')[0];
+		prefixes.forEach(prefix => urls.push(prefix + video_id));
+		return urls;
+	}
+	return [url];
+};
+
+const sortPosts = function (posts, method) {
+	return posts.sort((a, b) => {
+		switch (method) {
+			case "Score":
+				a = a.data.score;
+				b = b.data.score;
+				break;
+			case "Comments":
+				a = a.data.num_comments;
+				b = b.data.num_comments;
+				break;
+			case "Date":
+				a = a.data.created_utc;
+				b = b.data.created_utc;
+				break;
+		}
+		return a < b ? 1
+			: a > b ? -1
+			: 0;
+	});
+};
+
+var posts = [];
+const showPosts = function () {
+	let method = document.getElementById("sortMethod").value;
+	let resultDiv = document.getElementById("results");
+	resultDiv.innerHTML = "";
+	sortPosts(posts, method)
+		.forEach(post => resultDiv.innerHTML += linkTemplate(post.data));
+};
 
 const searchReddit = function () {
 	let redditURL = "http://www.reddit.com/api/info.json?url=";
 	let urlToSearch = document.getElementById("urlInput").value;
 	let resultDiv = document.getElementById("results");
+	let header = document.getElementById("resultHeader");
+	let spinner = new Spinner();
+	let promises = [];
 
-	spinDiv.style.display = "block";
-	resultDiv.innerHTML = "";
+	spinner.spin(document.getElementById("spinner"));
+	posts = [];
 
 	getURLs(urlToSearch).forEach(url => {
-		$.getJSON(redditURL + url, data => {
-			data.data.children.forEach(post => {
-				resultDiv.innerHTML += linkTemplate(post.data);
-			});
-		});
+		promises.push(new Promise(resolve => {
+			$.getJSON(redditURL + url)
+				.done(result => resolve(result.data.children));
+		}));
 	});
-	setTimeout(() => {
-		spinDiv.style.display = "none";
-		if (resultDiv.innerHTML === "") {
-			resultDiv.innerHTML = `<div class="alert alert-info">It looks like that link hasn't been posted anywhere on Reddit yet.</div>`;
+
+	Promise.all(promises).then(results => {
+		results.forEach(result => result.forEach(post => posts.push(post)));
+		if (posts.length === 0) {
+			resultDiv.innerHTML = `
+				<div class="alert alert-info">
+					That link hasn't been posted anywhere on Reddit yet!
+				</div>`;
+		} else {
+			header.style.visibility = "visible";
+			header.style.display = "flex";
+			document.getElementById("numResults").innerHTML =
+				posts.length + " results";
+			showPosts(posts);
 		}
-	}, 3000);
+		spinner.stop();
+	});
 };
 
-$(document).ready(function () {
-	spinDiv.style.display = "none";
-	spinner.spin(spinDiv);
-	$("#urlInput").keypress(e => {
-		if (e.keyCode == 13)
-			searchReddit();
-	});
-});
+
+// misc ======================================================================
+
+// If the user presses [Enter], perform a search
+document.getElementById("urlInput").onkeypress = e => {
+	if (e.keyCode == 13) searchReddit();
+};
+document.getElementById("searchButton").onclick = e => searchReddit();
+document.getElementById("sortMethod").onchange = e => showPosts();
